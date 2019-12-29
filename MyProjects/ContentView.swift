d3e9001c42833026e601de6d55cfab38215e98a8
Fırat <firat.ozkms@gmail.com>
2019-12-29 21:36:31 +0300

    Initial Commit

---
+++ MyProjects/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyProjects
+//
+//  Created by Firot on 29.12.2019.
+//  Copyright © 2019 Firot. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
