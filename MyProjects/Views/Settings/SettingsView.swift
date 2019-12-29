1da9ce1fba452e8ba4a3826dc254a634e2f913a3
Fırat <firat.ozkms@gmail.com>
2019-12-29 21:39:54 +0300

    first

---
+++ MyProjects/Views/Settings/SettingsView.swift
@@ -0,0 +1,21 @@
+//
+//  SettingsView.swift
+//  MyProjects
+//
+//  Created by Firot on 29.12.2019.
+//  Copyright © 2019 Firot. All rights reserved.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
