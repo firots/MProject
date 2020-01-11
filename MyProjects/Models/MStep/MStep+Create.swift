af0c090e0e1570c3788efed020451dd0c008e648
Fırat <firat.ozkms@gmail.com>
2020-01-11 17:26:53 +0300

    ...

---
+++ MyProjects/Models/MStep/MStep+Create.swift
@@ -0,0 +1,22 @@
+//
+//  MStep+Bind.swift
+//  MyProjects
+//
+//  Created by Firot on 11.01.2020.
+//  Copyright © 2020 Firot. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+extension MStep {
+    static func create(from model: StepCellViewModel, context moc: NSManagedObjectContext) -> MStep {
+        let step = MStep(context: moc)
+        step.id = model.id
+        step.name = model.name
+        step.done = model.done
+        step.created = model.created
+        step.task = model.task
+        return step
+    }
+}
