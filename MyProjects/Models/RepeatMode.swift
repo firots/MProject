626994829519a1cbe22abdc99e4dbcb903823b92
Fırat <firat.ozkms@gmail.com>
2020-01-16 00:39:42 +0300

    ...

--- MyProjects/Models/RepeatMode.swift
+++ MyProjects/Models/RepeatMode.swift
@@ -8,20 +8,48 @@
 
 import Foundation
 
-public enum RepeatMode: String {
+public enum RepeatMode: Int {
     case none
-    case daily
+    case hour
+    case day
     case dow
     case dom
     case wdom
     
-    static var all = [none, daily, dow, dom, wdom]
+    static var names = ["none", "hourly", "daily", "dow", "dom", "wdom"]
+    
+    static var all = [none, hour, day, dow, dom, wdom]
     
     static var descriptions = [
         "Does not repeat",
+        "Repeats every hour",
         "Repeats every day",
         "Repeats every week of day",
         "Repeats every day of month",
         "Repeats every day of week of month"
     ]
+}
+
+struct MNotificationModel {
+    /* uuid will be transferred to notification center */
+    var id: UUID
+    
+    /* can belong to project or task*/
+    var project: MProject?
+    var task: MTask?
+    
+    /* callback date date for non repeating notifications */
+    var date: Date?
+    
+    
+    /* repeat section */
+    var repeatMode: RepeatMode?
+    
+    var startDate: Date?
+    var endDate: Date?
+    var repeatMinute: Int
+    var repeatHour: Int
+    
+    var repeatInterval: Int
+    
 }
